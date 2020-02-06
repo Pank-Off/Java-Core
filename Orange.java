5876b6dffc26db9f276455ebe1370643f48d2882
Pank-Off <59021891+Pank-Off@users.noreply.github.com>
2020-02-06 12:44:13 +0300

    Create Orange.java

---
+++ Orange.java
@@ -0,0 +1,10 @@
+package com.geekbrains.generic;
+
+public class Orange extends Fruit {
+    private static double weight = 1.5f;
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+}
