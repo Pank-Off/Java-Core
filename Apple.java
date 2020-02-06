0c2103b80ccc5173c9b35d8eb86cf70b1358cffc
Pank-Off <59021891+Pank-Off@users.noreply.github.com>
2020-02-06 12:44:29 +0300

    Create Apple.java

---
+++ Apple.java
@@ -0,0 +1,10 @@
+package com.geekbrains.generic;
+
+public class Apple extends Fruit {
+    private static double weight = 1.0f;
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+}
