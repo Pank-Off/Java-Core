0f600f7f7a59e3c3312693b87d185e0fa2571cc7
Pank-Off <pankov.kirill1997@yandex.ru>
2020-02-27 23:26:16 +0300

    Сетевой чат. Логирование, потоки ввода-вывода, JDBC.

---
+++ january-chat/server/src/main/java/com/geekbrains/chat/server/AuthManager.java
@@ -0,0 +1,6 @@
+package com.geekbrains.chat.server;
+
+public interface AuthManager {
+    String getNicknameByLoginAndPassword(String login, String password);
+    void changeNick(String oldnickname, String newnickname);
+}
